6c9878257e1dedf143fa6293af1fbd245dcf9656
senthilanand92 <senthilanand92@gmail.com>
2018-05-05 02:58:58 +0530

    addind code for adding products

--- EcommerceApp/src/app/modules/product/service/product.service.ts
+++ EcommerceApp/src/app/modules/product/service/product.service.ts
@@ -1,26 +1,32 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class ProductService {
 
   Products = [
-{id: '1', productname: 'Camera' , productDescription: 'This is used to take photos', productPrice: '20$' },
-{id: '2', productname: 'Phone' , productDescription: 'This is used to call people and take photos wohoo', productPrice: '40$' },
-{id: '3', productname: 'Laptop' , productDescription: 'This is used to call people and take photos and develop your applications'
-, productPrice: '60$' }
+    { productname: 'Camera1' , productDescription: 'This is used to take photos', productPrice: '20$' },
+    { productname: 'Phone1' , productDescription: 'This is used to call people and take photos wohoo', productPrice: '40$' },
+    { productname: 'Laptop1' , productDescription: 'This is used to call people and take photos and develop your applications'
+    , productPrice: '60$' }
   ];
 
   constructor(private httpclient: HttpClient) { }
 
   getProducts(): Observable<any[]> | any | boolean {
 
-    const allProducts = this.httpclient.get('https://products-967f5.firebaseio.com/.json',{
+    const allProducts = this.httpclient.get('https://products-967f5.firebaseio.com/.json', {
       observe: 'body',
       responseType: 'json'
   });
-    return allProducts;
+
+ return allProducts;
+  }
+
+  addProducts(products: any[]) {
+    const req = new HttpRequest('PUT', 'https://products-967f5.firebaseio.com/.json', products, {reportProgress: true});
+    return this.httpclient.request(req);
   }
 
 }
--- EcommerceApp/src/app/modules/product/viewproducts/viewproducts.component.ts
+++ EcommerceApp/src/app/modules/product/viewproducts/viewproducts.component.ts
@@ -8,17 +8,36 @@ import {ProductService} from '../service/product.service';
 })
 export class ViewproductsComponent implements OnInit {
 
-  Products = [];
+  products = [];
+
+  moreProducts = [
+    { productname: 'Camera4' , productDescription: 'This is used to take photos', productPrice: '20$' },
+    { productname: 'Phone4' , productDescription: 'This is used to call people and take photos wohoo', productPrice: '40$' },
+    { productname: 'Laptop4' , productDescription: 'This is used to call people and take photos and develop your applications'
+    , productPrice: '60$' }
+  ];
+
 
   constructor(private productService: ProductService) { }
 
   ngOnInit() {
-     this.productService.getProducts().subscribe(
-     (data: any[]) => {
-       console.log(data);
-      this.Products =  data;
+      this.productService.getProducts().subscribe(
+      (data: any[]) => {
+        console.log(data)
+        this.products = data;
+      }
+      );
     }
-    );
-  }
 
+    addProduct() {
+      alert("adding product");
+      let tempProducts = [];
+      tempProducts.push(...this.products);
+      tempProducts.push(...this.moreProducts);
+      console.log(tempProducts)
+      this.productService.addProducts(tempProducts).subscribe(
+        (data) => console.log(data),
+        (error) => console.log(error)
+      );
+    }
 }
